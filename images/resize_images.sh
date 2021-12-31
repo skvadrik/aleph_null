15b2814c8aa4fb85fa9a01b5d1c249275a061a99
Ulya Trofimovich <skvadrik@gmail.com>
2021-12-31 22:10:09 +0000

    sync skvadrik.github.io/aleph_null.git:114fc18: Tweak CSS. (2021-12-31 22:06:47 +0000)
    Signed-off-by: Ulya Trofimovich <skvadrik@gmail.com>

---
+++ images/resize_images.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]
+then
+    echo "usage: ./resize_images.sh <folder>"
+    exit 1
+fi
+
+for f in "$1"/*
+do
+    convert -resize 1200x900 $f $f
+    # convert -resize 1000x750 $f $f
+    # convert -resize 800x600 $f $f
+    # convert -resize 720x540 $f $f
+done
